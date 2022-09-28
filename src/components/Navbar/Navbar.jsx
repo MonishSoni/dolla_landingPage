4b0f323af3539224f13f371e9ea60006619738f5
Monish Soni <monishsoni27@gmail.com>
2022-09-28 21:46:48 +0530

    done till sidebar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Nav, NavbarContainer, Navlogo, MobileIcon, NavMenu, Navitem, Navlinks,NavBtn,NavBtnlink } from './style';
+import { HiMenuAlt2 } from "react-icons/hi";
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    {/* logo ======================== */}
+                    <Navlogo to='/'>dolla</Navlogo>
+                    {/* menu =========================*/}
+                    <NavMenu>
+                        <Navitem><Navlinks to='about'>About</Navlinks></Navitem>
+                        <Navitem><Navlinks to='discover'>Discover</Navlinks></Navitem>
+                        <Navitem><Navlinks to='services'>Services</Navlinks></Navitem>
+                        <Navitem><Navlinks to='signup'>Signup</Navlinks></Navitem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnlink to='/signin'>Sign In</NavBtnlink>
+                    </NavBtn>
+                    {/* icon ========================== */}
+                    <MobileIcon>
+                        <HiMenuAlt2 />
+                    </MobileIcon>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CloseIcon, Icon, SidebarContainer } from './style';
+
+const Sidebar = () => {
+    return (
+        <>
+            <SidebarContainer>
+                <Icon>
+                    <CloseIcon />
+                </Icon>
+            </SidebarContainer>
+        </>
+    )
+}
+
+export default Sidebar
